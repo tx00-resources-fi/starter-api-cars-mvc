3ccc1dc574866e39d846402cc0935188059d24e9
Sami <sami.bencheikh@metropolia.fi>
2024-09-09 15:32:57 +0300

    update

--- models/carModel.js
+++ models/carModel.js
@@ -54,34 +54,6 @@ function deleteOneById(id) {
   return false; // Return false if the item was not found
 }
 
-// if (require.main === module) {
-//   let result = addOne({
-//     model: "Corolla",
-//     color: "Red",
-//     age: 3,
-//   });
-//   console.assert(result.length === 1, "Test 1 Failed: Should add one car");
-//   console.log(result);
-//   // Test 2: Add another valid car
-//   result = addOne({
-//     model: "Civic",
-//     color: "Blue",
-//     age: 2,
-//   });
-//   console.assert(result.length === 2, "Test 2 Failed: Should add another car");
-//   console.log(result);
-
-//   console.log("getAll called:", getAll());
-
-//   console.log("findById called:", findById(1));
-
-//   console.log("updateOne called:", updateOneById(1, { age: 4, color: "Green" }));
-//   console.log("findById called after item updated:", findById(1));
-
-//   console.log("deleteOneById called:", deleteOneById(1));
-//   console.log("findById called after item deleted:", findById(1));
-// }
-
 if (require.main === module) {
   // Add car
   let result = addOne({ model: "Corolla", color: "Red", age: 3 });
